4ba17e0f1f19c6fd6d19e72b42b0444f75b9e9ca
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2019-04-20 15:54:11 +0700

    azure connection DB

--- src/ternak/lele/controllers/LoginController.java
+++ src/ternak/lele/controllers/LoginController.java
@@ -7,6 +7,10 @@ package ternak.lele.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXPasswordField;
@@ -20,12 +24,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import ternak.lele.helpers.DBConnection;
+import ternak.lele.models.User;
 
 /**
  *
  * @author fian
  */
 public class LoginController implements Initializable {
+
+    private Connection connection = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
 
     @FXML
     private JFXTextField usernameField;
@@ -38,12 +48,49 @@ public class LoginController implements Initializable {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        if(username.equals("pemilik")){
+//        int aktor = getLoginAktor(username, password);
+
+        int aktor = User.getLoginValue(connection, preparedStatement, resultSet, username, password);
+
+        System.out.println(aktor);
+
+        if(aktor == 1){
             changePage(event, "pemilik");
-        } else {
+        } else if (aktor == 2){
             changePage(event, "peternak");
+        } else {
+            System.out.println("Username dan password salah");
         }
+
+//        if(username.equals("pemilik")){
+//            changePage(event, "pemilik");
+//        } else {
+//            changePage(event, "peternak");
+//        }
     }
+
+//    private int getLoginAktor(String username, String password){
+//        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+//
+//        try {
+//            preparedStatement = connection.prepareStatement(query);
+//            preparedStatement.setString(1, username);
+//            preparedStatement.setString(2, password);
+//
+//            resultSet = preparedStatement.executeQuery();
+//
+//            if(!resultSet.next()){
+//                return 0;
+//            } else {
+//                return resultSet.getInt("id_level");
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return 0;
+//    }
 
     private void changePage(ActionEvent event, String aktor){
         Node node = (Node) event.getSource();
@@ -63,6 +110,7 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        connection = DBConnection.connectDB();
+    }
     
 }
